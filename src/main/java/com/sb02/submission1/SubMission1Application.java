3f61b45ae73e7119e125687f1926bde7e16e9059
Seong-taeHan <kmljjn22@gmail.com>
2025-03-24 21:14:13 +0900

    코드잇 서브 미션 깃 초기 설정

---
+++ src/main/java/com/sb02/submission1/SubMission1Application.java
@@ -0,0 +1,13 @@
+package com.sb02.submission1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubMission1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubMission1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sb02/submission1/SubMission1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb02.submission1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubMission1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
